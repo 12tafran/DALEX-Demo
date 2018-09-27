05caed82ec78d765ec51fd84bc51c84463763dcc
Frankie <frankietang94@gmail.com>
2018-09-26 23:52:52 -0400

    minor edits

--- build.R
+++ build.R
@@ -1,4 +1,4 @@
 rmarkdown::render("presentation.Rmd",
                   output_file = rprojroot::find_rstudio_root_file("dist/index.html"))
 fs::dir_delete("dist/img")
-rmarkdown::render_supporting_files("static/img", "public")
+rmarkdown::render_supporting_files("static/img", "dist")
